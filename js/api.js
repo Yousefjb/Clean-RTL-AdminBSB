d0ea4a103a0e51195e73542243af235ed8a6a381
Yousef Balout <yousefjb@outlook.com>
2016-12-07 15:42:54 +0200

    api init

---
+++ js/api.js
@@ -0,0 +1,100 @@
+function customers(){
+	
+	this.get = function (){		
+	}
+	
+	this.get = function (id){		
+	}
+	
+	this.employees = function (customer){
+	}
+	
+	this.vouchers = function (customer){
+	}
+	
+	this.first = function (){		
+	}
+	
+	this.last = function (){		
+	}
+	
+	this.next = function (current){		
+	}
+	
+	this.prev = function (current){		
+	}
+
+	this.save = function (customer){		
+	}
+}
+
+function employees(){
+	
+	this.get = function (){		
+	}
+	
+	this.get = function (id){
+	}
+	
+	this.first = function (){		
+	}
+	
+	this.last = function (){		
+	}
+	
+	this.next = function (current){		
+	}
+	
+	this.prev = function (current){		
+	}
+
+	this.save = function (employee){
+	}
+}
+
+function vouchers(){
+	
+	this.get = function (){		
+	}
+	
+	this.get = function (id){
+	}
+	
+	this.first = function (){		
+	}
+	
+	this.last = function (){		
+	}
+	
+	this.next = function (current){		
+	}
+	
+	this.prev = function (current){		
+	}
+
+	this.save = function (voucher){
+	}
+}
+
+function zones(){
+	this.get = function (){		
+	}
+	
+	this.get = function (id){
+	}
+	
+	this.first = function (){		
+	}
+	
+	this.last = function (){		
+	}
+	
+	this.next = function (current){		
+	}
+	
+	this.prev = function (current){		
+	}
+
+	this.save = function (zone){
+	}
+}
